abb1799f1f3fb47752734bcd4cf7277574fa9ef8
CaioVicenzi <80120724+CaioVicenzi@users.noreply.github.com>
2022-10-28 20:09:10 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,103 +1,96 @@
-// NOME: Caio Oliveira Marques
+/*
+	AUTOR: Caio Oliveira Marques
+*/
 
+// importando as bibliotecas
 #include<stdio.h>
-#include<locale.h>
 #include<stdlib.h>
 
+// definindo o codigo de cada uma das cores que sera apresentadas no termianal, esses codigos s�o estaticos
 #define VERMELHO     "\x1b[31m"
 #define VERDE   "\x1b[32m"
 #define AZUL    "\x1b[34m"
 #define RESET   "\x1b[0m"
 
 int main(){
-	setlocale(LC_ALL, "Portuguese");
-
-	int quantidadeAssentos;
-	char nomeUsuario[30];
-	int idadeUsuario;
-	int opcao;
-	char nomeDestino[30];
+	int quantidadeAssentos = 0, idadeUsuario = 0, opcao = 0, linha, coluna;
+	char nomeUsuario[30], nomeDestino[30];
 	float preco = 0;
 	
-	// cadastrando o usuário
-	printf("Olá usuário! Bem vindo ao sistema de vendas da SALUMAN LINHAS AÉREAS\n\n");
+	// cadastrando o usuario (recebendo dados do nome e idade do usuario)
+	printf("Bem vindo ao sistema de vendas da SALUMAN LINHAS AEREAS\n\n");
 	printf("Digite aqui o seu nome: ");
 	fgets(nomeUsuario, 30, stdin);
 	printf("Digite aqui a sua idade: ");
 	scanf("%i", &idadeUsuario);
 	fflush(stdin);
 	
-	printf("Usuário %s registrado com sucesso!\n\n", nomeUsuario);
+	printf("Usuario registrado com sucesso\n\n", nomeUsuario);
 	system("PAUSE");
 	system("CLS");
 	
-	// Nesse ponto aqui nós temos em mãos o nome e a idade do usuário, que será usado posteriormente no cálculo do preço
-	
-	// Selecionando um avião:
-	printf("============ ESCOLHER AVIÃO ============\n\n");
+	// Nesta parte do codigo, informacoes sobre o aviao sao coletadas
+	printf("============ ESCOLHER AVIAO ============\n\n");
 	
 	printf("Digite o nome do destino: ");
 	fgets(nomeDestino, 30, stdin);
 	
-	printf("Agora, digite o preço: ");
+	printf("Agora, digite o preco: ");
 	scanf("%f", &preco);
 	fflush(stdin);
 	
-	// Se a idade do usuário for menor ou igual a 5, o preço será descontado na metade do valor.
-	if(idadeUsuario <=5)
-		preco = preco / 2;
+	// Se a idade do usuario for menor ou igual a 5, o preco sera descontado na metade do valor.
+	if(idadeUsuario <=5) preco = preco / 2;
 
-	//Nesse ponto nós temos a informação do nome do destino e preço, que usaremos posteriormente.
+	//Nesse ponto nos temos a informacao do nome do destino e preco, que usaremos posteriormente.
 	
-	while(true){
+	while(1){
 		printf("Para finalizar, digite a quantidade de assentos: ");
 		scanf("%i", &quantidadeAssentos);
 		fflush(stdin);
 		
-		// Como terceira condição necessária eu defini que a quantidade de assentos tem que ser divisível por 4.
+		// o minimo de assentos dos avioes da salumar sao 90, e o maximo sao 200
+		// todos os avioes da salumar tem a quantidade de assentos como sendo divisiveis por 4
 		if(quantidadeAssentos >= 90 && quantidadeAssentos <= 200 && quantidadeAssentos%4 == 0) break;
 			else {
-				printf(VERMELHO "Valor digitado é inválido, todos nossos aviões possuem entre 90 e 200 assentos e são divisíveis por 4...!\n");	
+				printf(VERMELHO "Valor digitado e invalido, todos nossos avioes possuem entre 90 e 200 assentos e a quantidade desses assentos eh divisivel por 4...!\n");	
 				printf(RESET);	
 			}
 	}
 	
-	// Nessa parte abaixo, a gente divide a quantidade de assentos em 4 filas de assentos, por isso o número de assentos, nessa
-	// empresa fictícia, é sempre divisível por 4.
+	// Nessa parte abaixo, a gente divide a quantidade de assentos em 4 filas de assentos, por isso o numero de assentos, nessa
+	// empresa ficticia, sao sempre divisiveis por 4.
 		
 	int quantidadeColunas = quantidadeAssentos/4;
 	
-	// O mapa dos assentos do avião é uma matriz de caracteres com sempre 4 filas, porém a quantidade de colunas varia de
+	// O mapa dos assentos do aviao eh uma matriz de caracteres com sempre 4 filas, porem a quantidade de colunas varia de
 	//acordo com a quantidade de assentos.
 	char assentos[4][quantidadeColunas]; 
 	
-	//preechendo as colunas com 'D' de disponíveis.
-	for(int linha = 0; linha<4; linha++){
-		for (int coluna = 0; coluna<quantidadeColunas; coluna++){
+	//preechendo as colunas com 'D' de disponiveis.
+	for(linha = 0; linha<4; linha++){
+		for (coluna = 0; coluna<quantidadeColunas; coluna++){
 			assentos[linha][coluna] = 'D';
 		}
 	}
 	system("CLS");
 	
-	// Essa matriz funcionará da seguinte forma: o primeiro valor será o índice, que começará por 1, 
-	// já o segundo, será a linha do assento e o terceiro, a coluna do assento.
+	// Essa matriz funcionara da seguinte forma: o primeiro valor sera o indice, que comecara por 1, 
+	// ja o segundo, sera a linha do assento e o terceiro, a coluna do assento.
 	int matrizReservas[quantidadeAssentos][3];
 	
-	int quantasReservasJaForamFeitas = 0;
-	int opcaoEscolhida;
-	int numeroIdentificadorReserva, i, linha, coluna;
-	bool valorFoiEncontrado = false;
+	int quantasReservasJaForamFeitas = 0, valorFoiEncontrado = 0, opcaoEscolhida, numeroIdentificadorReserva, i, j;
+	int quantidadeReservados, quantidadeConfirmados, quantidadeDisponiveis, colunas;
 	char confirmar;
-	int quantidadeReservados, quantidadeConfirmados, quantidadeDisponiveis;
 
-	// Só sai desse laço se o usuário digitar 4.
-	while(true){
+	// So sai desse laco se o usuario digitar 4.
+	while(1){
 		// Menu principal
 		printf("---- Menu Principal ----\n\n");
 		printf("1- Reservar assento\n");
 		printf("2- Confirmar uma reserva\n");
 		printf("3- Cancelar uma reserva\n");
-		printf("4- Informações do avião\n");
+		printf("4- Informacoes do aviao\n");
 		printf("5- Sair do Programa\n");
 		printf("Sua escolha: ");
 		scanf("%i", &opcaoEscolhida);
@@ -105,16 +98,16 @@ int main(){
 		switch (opcaoEscolhida){
 			case 1: 
 				system("CLS");
-				// Exibe o mapa dos assentos do avião
+				// Exibe o mapa dos assentos do aviao
 				printf("========> MAPA DOS ASSENTOS <========\n\n");
 				printf("|Colunas:|");
-				for(int colunas = 0; colunas<quantidadeColunas; colunas++){
+				for(colunas = 0; colunas<quantidadeColunas; colunas++){
 					printf("%2i|", colunas + 1);
 				}
 				printf("\n");
 				for(i = 0; i<4; i++){
 					printf("|Linha %i:|", i + 1);
-					for (int j = 0; j < quantidadeColunas; j++){
+					for (j = 0; j < quantidadeColunas; j++){
 						if(assentos[i][j] == 'D'){
 							printf(VERDE "%c", assentos[i][j]);
 							printf(RESET);
@@ -131,81 +124,81 @@ int main(){
 					}
 					printf("\n");
 				}
-				printf("PS:\nD = ASSENTO DISPONÍVEL \nR = ASSENTO RESERVADO\nC = ASSENTO COM RESERVA CONFIRMADA\n");
+				printf("PS:\nD = ASSENTO DISPONIVEL \nR = ASSENTO RESERVADO\nC = ASSENTO COM RESERVA CONFIRMADA\n");
 				
-				// Se a linha do assento digitada for válida o laço volta, caso contrário, o programa exibe uma mensagem de
-				//erro e o laço se repete, o programa pergunta novamente a linha do assento (processo semelhante acontece com
+				// Se a linha do assento digitada for valida o laco volta, caso contrario, o programa exibe uma mensagem de
+				//erro e o laco se repete, o programa pergunta novamente a linha do assento (processo semelhante acontece com
 				//a coluna
-				while(true){
-					// pedindo para o usuário as posições dos assentos
+				while(1){
+					// pedindo para o usuario as posicoes dos assentos
 					int linhaAssentoReservado, colunaAssentoReservado;
-					while(true){	
+					while(1){	
 						printf("Digite a linha do assento: ");
 						scanf("%i", &linhaAssentoReservado);
 						fflush(stdin);
 						if(linhaAssentoReservado <= 4 && linhaAssentoReservado > 0) break;
 							else{
-								printf( VERMELHO "Valor de linha digitado é inválido, existem apenas 4 linhas\n");
+								printf( VERMELHO "Valor de linha digitado eh invalido, existem apenas 4 linhas\n");
 								printf(RESET);
 							}
 					}
 	
-					while(true){
+					while(1){
 						printf("Digite a coluna do assento: ");
 						scanf("%i", &colunaAssentoReservado);
 						fflush(stdin);
 						if(colunaAssentoReservado <= quantidadeColunas && colunaAssentoReservado > 0) break;
-						else printf(VERMELHO "Valor de coluna digitado é inválido, existem apenas %i colunas\n", quantidadeColunas);
+						else printf(VERMELHO "Valor de coluna digitado eh invalido, existem apenas %i colunas\n", quantidadeColunas);
 						printf(RESET);
 					}
 		
-					// verificando se o assento está disponível
+					// verificando se o assento esta disponivel
 					if(assentos[linhaAssentoReservado-1][colunaAssentoReservado-1] == 'D'){
-						// trocando a posição para "Reservado"
+						// trocando a posicao para "Reservado"
 						assentos[linhaAssentoReservado-1][colunaAssentoReservado-1] = 'R'; 
-						// subtraí um da linha assento reservado e coluna para que fique fácil para o usuário ter um assento
+						// subtrai um da linha assento reservado e coluna para que fique facil para o usuario ter um assento
 						// que comece com 1 e termine com 4
 						
 						matrizReservas[quantasReservasJaForamFeitas][0] = quantasReservasJaForamFeitas + 1;
 						matrizReservas[quantasReservasJaForamFeitas][1] = linhaAssentoReservado - 1;
 						matrizReservas[quantasReservasJaForamFeitas][2] = colunaAssentoReservado - 1;
-						printf("SUA RESERVA POSSUI IDENTIFICAÇÃO %i, USE ESSA IDENTIFICAÇÃO PARA CONFIRMAR OU CANCELAR SUA RESERVA\n", 
+						printf("SUA RESERVA POSSUI IDENTIFICACAO %i, USE ESSA IDENTIFICACAO PARA CONFIRMAR OU CANCELAR SUA RESERVA\n", 
 							quantasReservasJaForamFeitas + 1);
-						printf("PREÇO: %.2f\n\n", preco);
+						printf("PRECO: %.2f\n\n", preco);
 						quantasReservasJaForamFeitas++;
-						printf(VERDE "--- Reserva concluída com sucesso! ---\n\n");
+						printf(VERDE "--- Reserva concluida com sucesso! ---\n\n");
 						printf(RESET);
 						
 						system("PAUSE");
 						system("CLS");
 						break;
-					} else printf("Assento já está reservado ou confirmado, tente outro assento\n");
+					} else printf("Assento ja esta reservado ou confirmado, tente outro assento\n");
 				}
 				break;
 	
 			case 2: 
-				valorFoiEncontrado = false;
+				valorFoiEncontrado = 0;
 				system("CLS");
 				printf("=======> CONFIRMAR UMA RESERVA <========\n\n");
-				printf("Digite o número identificador da reserva: ");
+				printf("Digite o numero identificador da reserva: ");
 				scanf("%i", &numeroIdentificadorReserva);
 				fflush(stdin);
 				
 				for(i = 0; i < quantasReservasJaForamFeitas; i++){
 					if (matrizReservas[i][0] == numeroIdentificadorReserva){
-						valorFoiEncontrado = true;
+						valorFoiEncontrado = 1;
 						linha = matrizReservas[i][1];
 						coluna = matrizReservas[i][2]; 
 						if(assentos[linha][coluna] == 'R') {
-							printf("\n\n------ INFORMAÇÕES DA RESERVA -------\n\n");
+							printf("\n\n------ INFORMA�OES DA RESERVA -------\n\n");
 							printf("Nome do passageiro: %s", nomeUsuario);
 							printf("Linha do assento: %i\n", linha + 1);
 							printf("Coluna do assento: %i\n", coluna + 1);
 							printf("Numero de reserva: %i\n", numeroIdentificadorReserva);
 							printf("Destino: %s\n", nomeDestino);
-							printf("Preço: %.2f\n\n", preco);
+							printf("Preco: %.2f\n\n", preco);
 							
-							printf("Deseja confirmar a reserva? (S -> SIM, QUALQUER OUTRO -> NÃO): ");
+							printf("Deseja confirmar a reserva? (S -> SIM, QUALQUER OUTRO -> NAO): ");
 							scanf("%c", &confirmar);
 							fflush(stdin);
 							
@@ -214,12 +207,12 @@ int main(){
 								printf(VERDE "RESERVA CONFIRMADA COM SUCESSO!\n\n");
 								printf(RESET);	
 							}	
-						} else printf("A RESERVA DIGITADA JÁ FOI CONFIRMADA OU FOI CANCELADA...\n\n");
+						} else printf("A RESERVA DIGITADA JA FOI CONFIRMADA OU FOI CANCELADA...\n\n");
 					}
 				}
 				
 				if(!valorFoiEncontrado) {
-					printf(VERMELHO "A RESERVA DIGITADA NÃO FOI ENCONTRADA!\n\n");
+					printf(VERMELHO "A RESERVA DIGITADA NAO FOI ENCONTRADA!\n\n");
 					printf(RESET);	
 				}
 				system("PAUSE");
@@ -227,28 +220,28 @@ int main(){
 				break;
 				
 			case 3: 
-				valorFoiEncontrado = false;
+				valorFoiEncontrado = 0;
 				system("CLS");
 				printf("=======> CANCELAR UMA RESERVA <========\n\n");
-				printf("Digite o número identificador da reserva: ");
+				printf("Digite o numero identificador da reserva: ");
 				scanf("%i", &numeroIdentificadorReserva);
 				fflush(stdin);
 				
 				for(i = 0; i < quantasReservasJaForamFeitas; i++){
 					if (matrizReservas[i][0] == numeroIdentificadorReserva){
-						valorFoiEncontrado = true;
+						valorFoiEncontrado = 1;
 						linha = matrizReservas[i][1];
 						coluna = matrizReservas[i][2]; 
 						if(assentos[linha][coluna] == 'R') {
-							printf("\n\n------ INFORMAÇÕES DA RESERVA -------\n\n");
+							printf("\n\n------ INFORMACOES DA RESERVA -------\n\n");
 							printf("Nome do passageiro: %s", nomeUsuario);
 							printf("Linha do assento: %i\n", linha + 1);
 							printf("Coluna do assento: %i\n", coluna + 1);
 							printf("Numero de reserva: %i\n", numeroIdentificadorReserva);
 							printf("Destino: %s\n", nomeDestino);
-							printf("Preço: %.2f\n\n", preco);
+							printf("Preco: %.2f\n\n", preco);
 							
-							printf("Deseja cancelar a reserva? (S -> SIM, QUALQUER OUTRO -> NÃO): ");
+							printf("Deseja cancelar a reserva? (S -> SIM, QUALQUER OUTRO -> NAO): ");
 							scanf("%c", &confirmar);
 							fflush(stdin);
 							
@@ -256,12 +249,15 @@ int main(){
 								assentos[linha][coluna] = 'D';
 								printf(VERDE "RESERVA CANCELADA COM SUCESSO!\n\n");
 								printf(RESET);	
+								
+								matrizReservas[i][0] = 0;
+								matrizReservas[i][1] = 0;
 							}	
-						} else printf("A RESERVA DIGITADA JÁ FOI CONFIRMADA OU FOI CANCELADA...\n\n");
+						} else printf("A RESERVA DIGITADA JA� FOI CONFIRMADA OU FOI CANCELADA...\n\n");
 					}
 				}
 				
-				if(!valorFoiEncontrado) printf(VERMELHO "A RESERVA DIGITADA NÃO FOI ENCONTRADA!\n\n");
+				if(!valorFoiEncontrado) printf(VERMELHO "A RESERVA DIGITADA NAO FOI ENCONTRADA!\n\n");
 				printf(RESET);
 				system("PAUSE");
 				system("CLS");
@@ -274,16 +270,16 @@ int main(){
 				float precoTotal;
 				
 				system("CLS");
-				// Exibe o mapa dos assentos do avião
+				// Exibe o mapa dos assentos do aviao
 				printf("========> MAPA DOS ASSENTOS <========\n\n");
 				printf("|Colunas:|");
-				for(int colunas = 0; colunas<quantidadeColunas; colunas++){
+				for(colunas = 0; colunas<quantidadeColunas; colunas++){
 					printf("%2i|", colunas + 1);
 				}
 				printf("\n");
 				for(i = 0; i<4; i++){
 					printf("|Linha %i:|", i + 1);
-					for (int j = 0; j < quantidadeColunas; j++){
+					for (j = 0; j < quantidadeColunas; j++){
 						if(assentos[i][j] == 'D'){
 							printf(VERDE "%c", assentos[i][j]);
 							printf(RESET);
@@ -301,10 +297,10 @@ int main(){
 					printf("\n");
 				}
 				
-				printf("PS:\nD = ASSENTO DISPONÍVEL \nR = ASSENTO RESERVADO\nC = ASSENTO COM RESERVA CONFIRMADA\n");
+				printf("PS:\nD = ASSENTO DISPONIVEL \nR = ASSENTO RESERVADO\nC = ASSENTO COM RESERVA CONFIRMADA\n");
 				
 				for(i = 0; i<4; i++){
-					for(int j = 0; j<quantidadeColunas; j++){
+					for(j = 0; j<quantidadeColunas; j++){
 						if(assentos[i][j] == 'C') quantidadeConfirmados++;
 							else if(assentos[i][j] == 'D') quantidadeDisponiveis++;
 								else quantidadeReservados++;
@@ -312,12 +308,12 @@ int main(){
 				}
 				
 				printf("\n\n");
-				printf("=========> INFORMAÇÕES <=========\n\n");
+				printf("=========> INFORMACOES <=========\n\n");
 				precoTotal = quantidadeConfirmados * preco;
 				printf("DESTINO: %s", nomeDestino);
 				printf("ASSENTOS RESERVADOS: %i\n", quantidadeReservados);
 				printf("RESERVAS CONFIRMADAS: %i\n", quantidadeConfirmados);
-				printf("ASSENTOS DISPONÍVEIS: %i\n", quantidadeDisponiveis);
+				printf("ASSENTOS DISPONIVEIS: %i\n", quantidadeDisponiveis);
 				printf("TOTAL EM REAIS: %.2f\n\n", precoTotal);
 				
 				system("PAUSE");
@@ -329,7 +325,7 @@ int main(){
 				return 0;
 			 
 			default: 
-				printf(VERMELHO "Valor inválido."); 
+				printf(VERMELHO "Valor invalido."); 
 				printf(RESET);
 				break;
 		}
